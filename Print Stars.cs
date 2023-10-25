1187768ef8f42b7f78ab5ed5f19ad7895742a991
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-25 10:08:06 +0300

    Print Stars

---
+++ Print Stars.cs
@@ -0,0 +1,33 @@
+﻿{
+    for (int i = 0; i < 20; i++)
+    {
+        PrintStars(i + 1);
+    }
+}
+void PrintStars(int count)
+{
+    Console.WriteLine(new string('*', count));
+}
+//static void PrintStars()
+//{
+//Console.WriteLine("********");
+//}
+//static void Main()
+//{
+//PrintStars();
+//PrintStars();
+//PrintStars();
+//PrintStars();
+//}
+
+//static void PrintStars(int count)
+//{
+//Console.WriteLine(new string('*', count));
+//}
+//static void Main()
+//{
+//PrintStars(1);
+//PrintStars(2);
+//PrintStars(3);
+//PrintStars(4);
+//}
