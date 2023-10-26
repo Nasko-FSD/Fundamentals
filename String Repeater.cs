890d5c08cecc924aff827aa6851eda233120fb97
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-26 10:36:52 +0300

    String Repeater

---
+++ String Repeater.cs
@@ -0,0 +1,11 @@
+﻿StringRepeater();
+
+static void StringRepeater()
+{
+    string Text = Console.ReadLine();
+    int num = int.Parse(Console.ReadLine());
+    for (int repeat = 1; repeat <= num; repeat++)
+    {
+        Console.Write($"{Text}");
+    }
+}
