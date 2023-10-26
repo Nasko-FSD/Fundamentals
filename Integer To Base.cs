0093c3947c3107d8f757f8a7d731908c2b49d092
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-26 18:56:46 +0300

    Integer To Base

---
+++ Integer To Base.cs
@@ -0,0 +1,16 @@
+﻿PrintIntegerToBase();
+
+static void PrintIntegerToBase()
+{
+    int number = int.Parse(Console.ReadLine());
+    int toBase = int.Parse(Console.ReadLine());
+    string result = string.Empty;
+    string remainder = string.Empty;
+    while (number > 0)
+    {
+        remainder = (number % toBase).ToString();
+        result = result.Insert(0, remainder);
+        number /= toBase;
+    }
+    Console.WriteLine(result);
+}
