50dc2a5ddca05c75fc109d1c28b2b5e9ccf177bc
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-27 09:34:05 +0300

    Blank Receipt

---
+++ Blank Receipt.cs
@@ -0,0 +1,23 @@
+﻿UpperPart();
+MiddlePart();
+LowerPart();
+
+static void UpperPart()
+{
+    Console.WriteLine("CASH RECEIPT");
+    Console.WriteLine("------------------------------");
+}
+
+
+static void MiddlePart()
+{
+    Console.WriteLine("Charged to____________________");
+    Console.WriteLine("Received by___________________");
+}
+
+
+static void LowerPart()
+{
+    Console.WriteLine("------------------------------");
+    Console.WriteLine("(c) SoftUni");
+}
