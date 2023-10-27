2f6ad55799253b3f85b00a82d663d27e416e2f71
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-27 11:59:00 +0300

    Math Power

---
+++ Math Power.cs
@@ -0,0 +1,8 @@
+﻿MathPower();
+
+static void MathPower()
+{
+    double n = double.Parse(Console.ReadLine());
+    double power = double.Parse(Console.ReadLine());
+    Console.WriteLine(Math.Pow(n, power));
+}
