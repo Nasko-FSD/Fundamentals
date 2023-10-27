e0b69866d2d1af26703697b6b6582063c6dafd3d
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-27 10:11:16 +0300

    Sign Of An Integer

---
+++ Sign Of An Integer.cs
@@ -0,0 +1,18 @@
+﻿SignOfAnInteger();
+
+static void SignOfAnInteger()
+{
+    int number = int.Parse(Console.ReadLine());
+    if (number > 0)
+    {
+        Console.WriteLine($"The number {number} is positive.");
+    }
+    if (number == 0)
+    {
+        Console.WriteLine($"The number {number} is zero.");
+    }
+    if (number < 0)
+    {
+        Console.WriteLine($"The number {number} is negative.");
+    }
+}
