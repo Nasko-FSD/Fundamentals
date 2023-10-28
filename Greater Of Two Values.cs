9ca488cbb73d9e359b10a8b3ac2dac395c79666b
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-28 11:49:10 +0300

    Greater Of Two Values

---
+++ Greater Of Two Values.cs
@@ -0,0 +1,41 @@
+﻿string DataType = Console.ReadLine();
+string FirstInput = Console.ReadLine();
+string SecondInput = Console.ReadLine();
+switch (DataType)
+{
+    case "int":
+        int FirstInt = int.Parse(FirstInput);
+        int SecondInt = int.Parse(SecondInput);
+        int ResultInt = PrintInt(FirstInt, SecondInt);
+        Console.WriteLine(ResultInt); break;
+    case "char":
+        char FirstChar = char.Parse(FirstInput);
+        char SecondChar = char.Parse(SecondInput);
+        char ResultChar = PrintChar(FirstChar, SecondChar);
+        Console.WriteLine(ResultChar); break;
+    case "string":
+        string FirstString = FirstInput;
+        string SecondString = SecondInput;
+        string ResultString = PrintString(FirstInput, SecondInput);
+        Console.WriteLine(ResultString); break;
+}
+static int PrintInt(int FirstInt, int SecondInt)
+{
+    return Math.Max(FirstInt, SecondInt);
+}
+static char PrintChar(char FirstChar, char SecondChar)
+{
+    return (char)Math.Max(FirstChar, SecondChar);
+}
+static string PrintString(string FirstString, string SecondString)
+{
+    int ResultString = FirstString.CompareTo(SecondString);
+    if (ResultString > 0)
+    {
+        return (FirstString);
+    }
+    else
+    {
+        return (SecondString);
+    }
+}
