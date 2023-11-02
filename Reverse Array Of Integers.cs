69d17cc9a850127f996ddff501e79f3c6e5f4b8b
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-11-02 20:52:30 +0200

    Reverse Array Of Integers

---
+++ Reverse Array Of Integers.cs
@@ -0,0 +1,16 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] numbers = new int[n];
+for (int arrayLength = 0; arrayLength < numbers.Length; arrayLength++)
+{
+    numbers[arrayLength] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < numbers.Length / 2; i++)
+{
+    var oldElement = numbers[i];
+    numbers[i] = numbers[numbers.Length - 1 - i];
+    numbers[numbers.Length - 1 - i] = oldElement;
+}
+foreach (var elements in numbers)
+{
+    Console.WriteLine(elements);
+}
