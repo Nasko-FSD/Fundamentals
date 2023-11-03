7751cd6c2174797b59e5fac89dbf3ee57a88c7be
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-11-03 16:09:49 +0200

    Day Of Week Arrays

---
+++ Day Of Week Arrays.cs
@@ -0,0 +1,11 @@
+﻿string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+int n = int.Parse(Console.ReadLine()); 
+if (n < 1 || n > 7)          
+{
+    Console.WriteLine("Invalid day!");
+}
+else
+{
+    var weekDay = days[n - 1];         
+    Console.WriteLine(weekDay);
+}
