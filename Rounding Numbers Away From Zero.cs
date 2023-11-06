804831eb1a5f0f9f9b1791a3930ba98ca80716c7
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-11-06 14:56:20 +0200

    Rounding Numbers Away From Zero

---
+++ Rounding Numbers Away From Zero.cs
@@ -0,0 +1,6 @@
+﻿var nums = Console.ReadLine().Split(new char[] { ' ', ',', ';' }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+for (int i = 0; i < nums.Length; i++)
+{
+    int rounded = (int)Math.Round(nums[i], MidpointRounding.AwayFromZero);
+    Console.WriteLine($"{nums[i]} => {rounded}");
+}
