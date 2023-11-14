9b7452bfa86eb78062b2685394dfa9c05ac34959
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-11-14 15:58:31 +0200

    Largest Common End Second Solve

---
+++ Largest Common End Second Solve.cs
@@ -0,0 +1,19 @@
+﻿string[] wordsOne = Console.ReadLine().Split(' ');
+string[] wordsTwo = Console.ReadLine().Split(' ');
+var count1 = 0;
+var count2 = 0;
+for (int i = 0; i < wordsOne.Length && i < wordsTwo.Length; i++)
+{
+    if (wordsOne[i] == wordsTwo[i])
+    {
+        count1++;
+    }
+}
+for (int j = 0; j < wordsOne.Length && j < wordsTwo.Length; j++)
+{
+    if (wordsOne[wordsOne.Length - j - 1] == wordsTwo[wordsTwo.Length - j - 1])
+    {
+        count2++;
+    }
+}
+Console.WriteLine(Math.Max(count1, count2));
