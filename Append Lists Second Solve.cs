566db0b675982efe3375ba61ad0ff49e981402ef
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-11-15 18:06:01 +0200

    Append Lists Second Solve

---
+++ Append Lists Second Solve.cs
@@ -0,0 +1,14 @@
+﻿var lists = Console.ReadLine().Split('|').ToList();
+var result = new List<string>();
+for (int i = lists.Count - 1; i >= 0; i --)
+{
+    var currentList = lists[i].Split(' ');
+    foreach(var item in currentList)
+    {
+        if (item != "")
+        {
+            result.Add(item);
+        }
+    }
+}
+Console.WriteLine(string.Join(" ", result));
