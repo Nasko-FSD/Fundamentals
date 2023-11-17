c0d4ac95fa12817a0040cf07b282a58cf766206c
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-11-17 15:52:58 +0200

    Count Numbers Second Solve

---
+++ Count Numbers Second Solve.cs
@@ -0,0 +1,16 @@
+﻿var numbers = Console.ReadLine().Split(' ').Select(long.Parse).ToList();
+numbers.Sort();
+for (int start = 0; start < numbers.Count; start++)
+{
+    int end;
+    for (end = start + 1; end < numbers.Count; end++)
+    {
+        if (numbers[start] != numbers[end])
+        {
+            break;
+        }
+    }
+    var count = end - start;
+    start = end - 1;
+    Console.WriteLine($"{numbers[start]} -> {count}");
+}
