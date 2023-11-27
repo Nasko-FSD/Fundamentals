d731e76d4af5ddbe75757d70499caa467cdf363e
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-11-27 17:26:33 +0200

    Count Letters in String

---
+++ Count Letters in String.cs
@@ -0,0 +1,13 @@
+﻿string inputText = Console.ReadLine().ToLower();
+int[] count = new int[inputText.Max() + 1];
+foreach (var letter in inputText)
+{
+    count[letter]++;
+}
+for (char letters = (char)0; letters < count.Length; letters++)
+{
+    if (count[letters] != 0)
+    {
+        Console.WriteLine($"{letters} -> {count[letters]}");
+    }
+}
