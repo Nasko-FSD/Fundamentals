f8435dabd90a47b1678734a245d1e1056612d26a
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-11-27 18:55:28 +0200

    Print a Receipt

---
+++ Print a Receipt.cs
@@ -0,0 +1,9 @@
+﻿var nums = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+Console.WriteLine(@"/------------------\");
+foreach (var num in nums)
+{
+    Console.WriteLine("|{0,17:f2} |", num);
+}
+Console.WriteLine(@"|------------------|");
+Console.WriteLine("| Total:{0,10:f2} |", nums.Sum());
+Console.WriteLine(@"\------------------/");
