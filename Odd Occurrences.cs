248c243c4225b672cfc696c9a51fb240eb67956e
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-12-04 18:46:41 +0200

    Odd Occurrences

---
+++ Odd Occurrences.cs
@@ -0,0 +1,22 @@
+﻿var words = Console.ReadLine().ToLower().Split(' ');
+var allWords = new Dictionary<string, int>();
+foreach (var word in words)
+{
+    if (!allWords.ContainsKey(word))
+    {
+        allWords.Add(word, 1);
+    }
+    else
+    {
+        allWords[word]++;
+    }
+}
+var result = new List<string>();
+foreach (var pair in allWords)
+{
+    if (pair.Value % 2 == 1)
+    {
+        result.Add(pair.Key);
+    }
+}
+Console.WriteLine(string.Join(", ", result));
