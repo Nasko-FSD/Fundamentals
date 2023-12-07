5cbedfdd3332784c59cf145422e39c1662b056b8
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-12-07 18:00:39 +0200

    Append Lists Fourth Solve

---
+++ Append Lists Fourth Solve.cs
@@ -0,0 +1,14 @@
+﻿var lists = Console.ReadLine().Split('|').ToList();
+var result = new List<string>();
+for (int i = lists.Count - 1; i >= 0; i--)
+{
+    var currentList = lists[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+    for (int j = 0; j < currentList.Count; j++)
+    {
+        if (currentList.Count > 0) // Skips empty items
+        {
+            result.Add(currentList[j]);
+        }
+    }
+}
+Console.WriteLine(string.Join(" ", result));
