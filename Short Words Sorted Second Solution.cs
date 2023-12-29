92b15f17a67b91c8ad1f456dccbc4a3c09b0d69f
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-12-29 15:22:25 +0200

    Short Words Sorted Second Solve

---
+++ Short Words Sorted Second Solution.cs
@@ -0,0 +1,9 @@
+﻿var separators = ". , : ; ( ) [ ] \" ' ! ? "
+    .ToArray();
+var inputText = Console.ReadLine()
+    .ToLower()
+    .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+    .Where(x => x.Length < 5)
+    .OrderBy(x => x)
+    .Distinct();
+Console.WriteLine(string.Join(", ", inputText));
