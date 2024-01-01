f888e1fce229c50e4bf163416e0deb423496a01f
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-01-01 17:35:29 +0200

    Objects and Classes Day of Week

---
+++ Objects and Classes Day of Week.cs
@@ -0,0 +1,6 @@
+﻿var dateElements = Console.ReadLine()
+    .Split('-')
+    .Select(int.Parse)
+    .ToArray();
+var date = new DateTime(dateElements[2], dateElements[1], dateElements[0]);
+Console.WriteLine(date.DayOfWeek);
