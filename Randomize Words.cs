dafe8e97dd49305f0a2fd6c43c3446cb503287ac
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-01-02 22:35:36 +0200

    Randomize Words

---
+++ Randomize Words.cs
@@ -0,0 +1,12 @@
+﻿var words = Console.ReadLine()
+    .Split(' ')
+    .ToList();
+Random rnd = new Random();
+var result = new List<string>();
+while (words.Count > 0)
+{
+    var randomPosition = rnd.Next(0, words.Count()); 
+    result.Add(words[randomPosition]);
+    words.RemoveAt(randomPosition);
+}
+Console.WriteLine(string.Join("\r\n", result));
