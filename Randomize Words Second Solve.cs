6540a026c65d763fe50bb118573d2e25bb5a5769
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-01-02 22:36:03 +0200

    Randomize Words Second Solve

---
+++ Randomize Words Second Solve.cs
@@ -0,0 +1,12 @@
+﻿var words = Console.ReadLine()
+    .Split(' ')
+    .ToArray();
+Random rnd = new Random();
+for (int i = 0; i < words.Length; i++)
+{
+    var randomPosition = rnd.Next(words.Length);
+    var oldPosition = words[i];
+    words[i] = words[randomPosition];
+    words[randomPosition] = oldPosition;
+}
+Console.WriteLine(string.Join("\r\n", words));
