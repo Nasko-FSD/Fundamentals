14465f9beb5353e9b4e6ebefabbda6e3beadf952
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-01-04 20:41:16 +0200

    Closest Two Points.cs

--- Closest Two Points.cs
+++ Closest Two Points.cs
@@ -4,33 +4,33 @@ class Point
     public int X { get; set; }
     public int Y { get; set; }
 
-    public override string ToString() // It's a action on level Object there is no static
+    public override string ToString() 
     {
-        return "Point(" + X + ", " + Y + ")"; // Printing the points
+        return "Point(" + X + ", " + Y + ")"; 
     }
 }
 class ClosestTwoPoints
 {
-    public Point p1 { get; set; } // Property from type Point
+    public Point p1 { get; set; } 
     public Point p2 { get; set; }
-    public double Distance { get; set; } // type double
+    public double Distance { get; set; } 
 }
 class Example
 {
     static void Main()
     {
-        Point[] points = ReadPoints();//First step is to read the points
-        var closestTwoPoints = FindClosestTwoPoints(points);//Second step to find closestTwoPoints from (points)
+        Point[] points = ReadPoints();
+        var closestTwoPoints = FindClosestTwoPoints(points);
 
         Console.WriteLine("{0:f3}", closestTwoPoints.Distance);
-        Console.WriteLine(closestTwoPoints.p1);//To write the points override otherwise prints "Point"
-        Console.WriteLine(closestTwoPoints.p2);//To write the points override otherwise prints "Point"
+        Console.WriteLine(closestTwoPoints.p1);
+        Console.WriteLine(closestTwoPoints.p2);
     }
 
     static ClosestTwoPoints FindClosestTwoPoints(Point[] points)
     {
         var minDistance = double.MaxValue;
-        ClosestTwoPoints result = null;//result is from type ClosestTwoPoints
+        ClosestTwoPoints result = null;
         for (int i = 0; i < points.Length; i++)
         {
             for (int j = i + 1; j < points.Length; j++)
@@ -51,10 +51,10 @@ class Example
         return result;
     }
 
-    static Point[] ReadPoints() // static -> Action on level Class
+    static Point[] ReadPoints() 
     {
-        var n = int.Parse(Console.ReadLine());//How many points we are gonna Enter
-        var points = new Point[n]; // Array from n Points
+        var n = int.Parse(Console.ReadLine());
+        var points = new Point[n]; 
         for (int i = 0; i < n; i++)
         {
             points[i] = ReadPoint();
