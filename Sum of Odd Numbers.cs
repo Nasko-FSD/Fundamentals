7263c06b6d42295d68b23bb882d056e6ea2bdd94
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-01-19 21:02:34 +0200

    Sum of Odd Numbers

---
+++ Sum of Odd Numbers.cs
@@ -0,0 +1,12 @@
+﻿int counter = int.Parse(Console.ReadLine());
+int sum = 0;
+int currentNumber = 1;
+int i = 0;
+while (i < counter)
+{
+    Console.WriteLine(currentNumber);
+    sum += currentNumber;
+    currentNumber += 2;
+    i++;
+}
+Console.WriteLine($"Sum: {sum}");
