d7bc3b2788c2904ff8e2682ae09694f4d421809b
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-02-05 15:46:19 +0200

    Vowel or Digit

---
+++ Vowel or Digit.cs
@@ -0,0 +1,18 @@
+﻿char symbol = char.Parse(Console.ReadLine());
+if (symbol == 'a' ||
+    symbol == 'o' ||
+    symbol == 'u' ||
+    symbol == 'e' ||
+    symbol == 'i' ||
+    symbol == 'y')
+{
+    Console.WriteLine("vowel");
+}
+else if (char.IsDigit(symbol))
+{
+    Console.WriteLine("digit");
+}
+else
+{
+    Console.WriteLine("other");
+}
