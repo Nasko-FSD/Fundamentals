db5df7358534692f4c926629d6216e3ac3a91c47
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-02-08 17:50:16 +0200

    Printing Triangle Second Solve

---
+++ Printing Triangle Second Solve.cs
@@ -0,0 +1,32 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+PrintTriangle(num);
+static void PrintTriangle(int num)
+{
+    PrintTopPart(num);
+    PrintBottomPart(num);
+}
+
+static void PrintBottomPart(int num)
+{
+    for (int row = num - 1; row >= 1; row--)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            Console.Write(col + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+static void PrintTopPart(int num)
+{
+    for (int row = 1; row <= num; row++)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            Console.Write(col + " ");
+        }
+        Console.WriteLine();
+    }
+}
