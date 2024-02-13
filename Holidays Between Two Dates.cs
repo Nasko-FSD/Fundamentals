577f9a88a40e053e4e56b2aebaf8137d86ddea1b
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-02-13 16:52:04 +0200

    Holidays Between Two Dates

---
+++ Holidays Between Two Dates.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+Console.WriteLine("Enter first date in format type: d.M.yyy");
+var startDate = DateTime.ParseExact(Console.ReadLine(),
+  "d.M.yyyy", CultureInfo.InvariantCulture);
+
+Console.WriteLine("Enter second date in format type: d.M.yyy");
+
+var endDate = DateTime.ParseExact(Console.ReadLine(),
+  "d.M.yyyy", CultureInfo.InvariantCulture);
+
+var holidaysCount = 0;
+
+for (var date = startDate; date <= endDate; date = date.AddDays(1))
+{
+    if (date.DayOfWeek == DayOfWeek.Saturday ||
+        date.DayOfWeek == DayOfWeek.Sunday)
+    {
+        holidaysCount++;
+    }
+}
+
+Console.WriteLine(holidaysCount);
