03fb2c2d2baf2c28451196b8d00993fa12629105
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-02-13 18:35:34 +0200

    Multiply Evens by Odds

---
+++ Multiply Evens by Odds.cs
@@ -0,0 +1,43 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int result = GetMultipleOfEvensAndOdds(number);
+Console.WriteLine(result);
+static int GetMultipleOfEvensAndOdds(int number)
+{
+    int result = 0;
+    number = Math.Abs(number);
+    int evenSum = GetSumOfEvenDigits(number);
+    int oddSum = GetSumofOddDigits(number);
+    result = evenSum * oddSum;
+    return result;
+}
+
+static int GetSumOfEvenDigits(int number)
+{
+    int evenNumbers = 0;
+    while (number > 0)
+    {
+        int lastDigit = number % 10;
+        if (lastDigit % 2 == 0)
+        {
+            evenNumbers += lastDigit;
+        }
+        number /= 10;
+    }
+    return evenNumbers;
+}
+
+static int GetSumofOddDigits(int number)
+{
+    int oddNumbers = 0;
+    while (number > 0)
+    {
+        int lastDigit = number % 10;
+        if (lastDigit % 2 == 1)
+        {
+            oddNumbers += lastDigit;
+        }
+        number /= 10;
+    }
+    return oddNumbers;
+}
