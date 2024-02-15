fb9cae60cb421daf2af35b1c8f2ae36580f43288
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-02-15 14:18:26 +0200

    Prime Checker

---
+++ Prime Checker.cs
@@ -0,0 +1,21 @@
+﻿long number = long.Parse(Console.ReadLine());
+bool result = IsPrime(number);
+Console.WriteLine(result);
+
+static bool IsPrime(long number)
+{
+    bool result = true;
+    if (number == 0 || number == 1)
+    {
+        result = false;
+    }
+    for (long i = 2; i <= Math.Sqrt(number); i++)
+    {
+        if (number % i == 0)
+        {
+            result = false;
+            break;
+        }
+    }
+    return result;
+}
