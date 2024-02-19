09018f7043ce3c6a3a82b91ef4d5820569e2d411
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-02-19 16:18:14 +0200

    Factorial Trailing Zeroes

---
+++ Factorial Trailing Zeroes.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+int number = int.Parse(Console.ReadLine());
+BigInteger factorial = 1;
+for (int i = 2; i <= number; i++)
+{
+    factorial *= i;
+}
+int counter = 0;
+while (factorial > 0)
+{
+    BigInteger digit = factorial % 10;
+    if (digit > 0)
+    {
+        break;
+    }
+    counter++;
+    factorial /= 10;
+}
+Console.WriteLine($"{counter} trailing zeroes");
