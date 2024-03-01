824f1a646bc78b0995bc5217a9c0e1011aafa4bb
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-03-01 07:49:33 +0200

    Debugging Exercise - Be Positive

---
+++ Debugging Exercise - Be Positive.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BePositive_broken
+{
+    public static void Main()
+    {
+        int countSequences = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < countSequences; i++)
+        {
+            string[] input = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var numbers = new List<int>();
+
+            for (int j = 0; j < input.Length; j++)
+            {
+                if (!input[j].Equals(string.Empty))
+                {
+                    int num = int.Parse(input[j]);
+                    numbers.Add(num);
+                }
+            }
+
+            int count = 0;
+            for (int j = 0; j < numbers.Count; j++)
+            {
+                int currentNum = numbers[j];
+
+                if (currentNum >= 0)
+                {
+                    Console.Write(currentNum + " ");
+                    count++;
+                }
+                else if (currentNum < 0 && j != numbers.Count - 1)
+                {
+                    currentNum += numbers[j + 1];
+
+                    if (currentNum >= 0)
+                    {
+                        Console.Write(currentNum + " ");
+                        count++;
+                    }
+                    j++;
+                }
+            }
+            if (count <= 0)
+            {
+                Console.WriteLine("(empty)");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
