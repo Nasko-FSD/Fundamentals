b2ea38aab2148e4ff5fc403c3bad95ef0ac3e4a7
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-03-03 20:40:14 +0200

    Sequence of Commands Second Solve

---
+++ Debugging Exercise - Sequence of Commands Second Solve.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+
+public class SequenceOfCommands_broken
+{
+    public static void Main()
+    {
+        int sizeOfArray = int.Parse(Console.ReadLine());
+
+        long[] array = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(long.Parse)
+            .ToArray();
+
+        string[] command = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
+
+
+        while (command[0] != "stop")
+        {
+            int[] args = new int[2];
+
+            if (command.Contains("add") ||
+                command.Contains("subtract") ||
+                command.Contains("multiply"))
+            {
+                args[0] = int.Parse(command[1]);
+                args[1] = int.Parse(command[2]);
+            }
+            PerformAction(array, command[0], args);
+
+            PrintArray(array);
+            Console.WriteLine();
+
+            command = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+        }
+    }
+
+    static void PerformAction(long[] array, string command, int[] args)
+    {
+        int index = args[0] - 1;
+        int value = args[1];
+
+        switch (command)
+        {
+            case "multiply":
+                array[index] *= value;
+                break;
+            case "add":
+                array[index] += value;
+                break;
+            case "subtract":
+                array[index] -= value;
+                break;
+            case "lshift":
+                ArrayShiftLeft(array);
+                break;
+            case "rshift":
+                ArrayShiftRight(array);
+                break;
+        }
+    }
+
+    private static void ArrayShiftRight(long[] array)
+    {
+        long lastElement = array[array.Length - 1];
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            array[i] = array[i - 1];
+        }
+        array[0] = lastElement;
+    }
+
+    private static void ArrayShiftLeft(long[] array)
+    {
+        long firstElement = array[0];
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            array[i] = array[i + 1];
+        }
+        array[array.Length - 1] = firstElement;
+    }
+
+    private static void PrintArray(long[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
