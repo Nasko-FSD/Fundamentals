938bbc0db4e1209aab6ed8da5dcfc1df4f8e47ae
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-03-04 18:38:43 +0200

    Largest Common End Third Solve

---
+++ Largest Common End Third Solve.cs
@@ -0,0 +1,21 @@
+﻿string[] firstArray = Console.ReadLine()
+    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+string[] secondArray = Console.ReadLine()
+    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+int minLength = Math.Min(firstArray.Length, secondArray.Length);
+int leftCounter = 0;
+int rightCounter = 0;
+
+for (int i = 0; i < minLength; i++)
+{
+    if (firstArray[i] == secondArray[i])
+    {
+        leftCounter++;
+    }
+    if (firstArray[firstArray.Length - i - 1] == secondArray[secondArray.Length - i - 1]) 
+    {
+        rightCounter++;
+    }
+}
+int biggestCounter = Math.Max(rightCounter, leftCounter);
+Console.WriteLine(biggestCounter);
