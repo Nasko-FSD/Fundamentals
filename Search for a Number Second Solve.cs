77d04e238f24cf71d050dba99fe85d536ac89797
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-03-20 15:04:49 +0200

    Search for a Number Second Solve

---
+++ Search for a Number Second Solve.cs
@@ -0,0 +1,35 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+string[] commands = Console.ReadLine()
+    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+int takeElement = int.Parse(commands[0]);
+int deleteElement = int.Parse(commands[1]);
+int findElement = int.Parse(commands[2]);
+
+for (int i = 0; i < deleteElement; i++)
+{
+    numbers.RemoveAt(0);
+}
+
+bool elementFound = false;
+foreach (int num in numbers)
+{
+    if (num == findElement)
+    {
+        elementFound = true;
+        break;
+    }
+}
+
+if (elementFound)
+{
+    Console.WriteLine("YES!");
+}
+else
+{
+    Console.WriteLine("NO!");
+}
