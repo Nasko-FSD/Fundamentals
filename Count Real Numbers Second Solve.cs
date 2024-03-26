375943e5beef477622c4c330f44ef52d143a2d60
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-03-26 21:59:09 +0200

    Count Real Numbers Second Solve

---
+++ Count Real Numbers Second Solve.cs
@@ -0,0 +1,26 @@
+﻿List<double> input = Console.ReadLine()
+    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(double.Parse)
+    .ToList();
+
+List<double> numbers = new List<double>();
+List<int> count = new List<int>();
+
+input.Sort();
+foreach (var number in input)
+{
+    if (numbers.Contains(number) == false)
+    {
+        numbers.Add(number);
+        count.Add(1);
+    }
+    else
+    {
+        int index = numbers.IndexOf(number);
+        count[index]++;
+    }
+}
+for (int i = 0; i < numbers.Count; i++)
+{
+    Console.WriteLine($"{numbers[i]} -> {count[i]}");
+}
