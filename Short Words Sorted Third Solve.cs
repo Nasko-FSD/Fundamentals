00a02e04377c20afbd0a5c5069a578a221228e12
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-03-29 17:46:20 +0200

    Short Words Sorted Third Solve

---
+++ Short Words Sorted Third Solve.cs
@@ -0,0 +1,27 @@
+﻿char[] separators = ".,:;()[]\"'\\/!? ".ToCharArray();
+List<string> inputWords = Console.ReadLine()
+    .ToLower()
+    .Split(separators)
+    .ToList();
+List<string> result = new List<string>();
+foreach (var word in inputWords)
+{
+    if (word.Length < 5 && word != "")
+    {
+        result.Add(word);
+    }
+}
+for (int i = 0; i < result.Count; i++)
+{
+    for (int j = i + 1; j < result.Count; j++)
+    {
+        if (string.Compare(result[i], result[j]) > 0)
+        {
+            string temp = result[i];
+            result[i] = result[j];
+            result[j] = temp;
+        }
+    }
+}
+result = result.Distinct().ToList();
+Console.WriteLine(string.Join(", ", result));
