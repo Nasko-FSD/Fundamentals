60dad1539f77822ae287055b458a6fe0a2aa5f38
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-04-02 12:00:33 +0300

    A Miner Task

---
+++ A Miner Task.cs
@@ -0,0 +1,46 @@
+﻿Dictionary<string, int> wareHouse = new Dictionary<string, int>();
+
+while (true)
+{
+    string inputData = "";
+    int inputQuantity;
+
+    for (int i = 0; ; i++)
+    {
+        if (i % 2 == 0)
+        {
+            inputData = Console.ReadLine();
+
+            if (inputData == "stop")
+            {
+                break;
+            }
+            if (!wareHouse.ContainsKey(inputData))
+            {
+                wareHouse.Add(inputData, 0);
+            }
+        }
+        else if (i % 2 == 1)
+        {
+            inputQuantity = int.Parse(Console.ReadLine());
+            int defaultValue = 0;
+            if (wareHouse.ContainsValue(defaultValue))
+            {
+                wareHouse[inputData] = inputQuantity;
+            }
+            else if (!wareHouse.ContainsValue(defaultValue))
+            {
+                wareHouse[inputData] += inputQuantity;
+            }
+        }
+    }
+
+    foreach (var product in wareHouse)
+    {
+        Console.WriteLine($"{product.Key} -> {product.Value}");
+    }
+    if (inputData == "stop")
+    {
+        break;
+    }
+}
