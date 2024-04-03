43a1f329947d47b9994ee6aba68c525f1e5ca939
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-04-03 14:43:34 +0300

    A Miner Task Second Solve

---
+++ A Miner Task Second Solve.cs
@@ -0,0 +1,22 @@
+﻿Dictionary<string, int> wareHouse = new Dictionary<string, int>();
+
+while (true)
+{
+    string resource = Console.ReadLine();
+    if (resource == "stop")
+    {
+        break;
+    }
+    int quantity = int.Parse(Console.ReadLine());
+
+    if (!wareHouse.ContainsKey(resource))
+    {
+        wareHouse.Add(resource, 0);
+    }
+    wareHouse[resource] += quantity;
+}
+
+foreach (var product in wareHouse)
+{
+    Console.WriteLine($"{product.Key} -> {product.Value}");
+}
