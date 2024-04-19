fb0ca701c4a29f36e93675a9b7e7cb96e643344b
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-04-19 17:13:34 +0300

    Randomize Words Third Solve

---
+++ Randomize Words Third Solve.cs
@@ -0,0 +1,13 @@
+﻿string[] words = Console.ReadLine()
+    .Split();
+Random random = new Random();
+
+for (int i = 0; i < words.Length; i++)
+{
+    int index = random.Next(0, words.Length);
+
+    var temp = words[i];
+    words[i] = words[index];
+    words[index] = temp;
+}
+Console.WriteLine(string.Join(Environment.NewLine, words));
