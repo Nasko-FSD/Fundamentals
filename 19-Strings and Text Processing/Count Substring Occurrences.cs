c55c3eb9a808d52f400ea3397f88eec26b9d0db3
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-06-24 15:34:25 +0300

    Count Substring Occurrences

---
+++ 19-Strings and Text Processing/Count Substring Occurrences.cs
@@ -0,0 +1,12 @@
+﻿string inputText = Console.ReadLine().ToLower();
+string pattern = Console.ReadLine().ToLower();
+int count = 0;
+int index = inputText.IndexOf(pattern);//What we are searching
+while( index != -1)
+{
+    count++;
+    index = inputText.IndexOf(pattern, index+1);// If it's only one argument it starts from where we found the pattern
+    // overflow
+
+}
+Console.WriteLine(count);
