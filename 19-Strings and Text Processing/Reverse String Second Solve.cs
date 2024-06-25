fb49e6c5a2f37d8d19e51314569808ba4f88cd2b
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-06-25 10:26:34 +0300

    Reverse String Second Solve

---
+++ 19-Strings and Text Processing/Reverse String Second Solve.cs
@@ -0,0 +1,7 @@
+﻿string inputText = Console.ReadLine();
+string reversedString = string.Empty;
+for (int i = inputText.Length - 1; i >= 0; i--)
+{
+    reversedString += inputText[i];
+}
+Console.WriteLine(reversedString);
