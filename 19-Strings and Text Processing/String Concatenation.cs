e206bd8a8eb4967186b0bc1083405112af3eb500
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-06-25 15:00:54 +0300

    String Concatenation

---
+++ 19-Strings and Text Processing/String Concatenation.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+using System.Text;
+Stopwatch timer = new Stopwatch();
+timer.Start();
+StringBuilder sB = new StringBuilder();
+for (int i = 0; i < 50000; i++)
+{
+    sB.Append(Convert.ToString(i, 2));
+}
+Console.WriteLine(sB.Length);
+Console.WriteLine(timer.Elapsed);
