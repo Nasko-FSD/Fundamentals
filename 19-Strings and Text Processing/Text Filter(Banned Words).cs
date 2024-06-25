0a722e117959c788a10b879396a3007d6f64631a
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-06-25 14:29:47 +0300

    Text Filter(Banned Words)

---
+++ 19-Strings and Text Processing/Text Filter(Banned Words).cs
@@ -0,0 +1,8 @@
+﻿string[] bannedWords = Console.ReadLine()
+    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+string inputText = Console.ReadLine();
+foreach (var word in bannedWords)
+{
+    inputText = inputText.Replace(word, new string('*', word.Length));
+}
+Console.WriteLine(inputText);
