65aaeecff2723af066c4fce3787ee466e4ef9d8d
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-06-25 16:12:33 +0300

    Palindromes

---
+++ 19-Strings and Text Processing/Palindromes.cs
@@ -0,0 +1,22 @@
+﻿string inputText = Console.ReadLine();
+string[] words = inputText
+    .Split(".,!? ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+List<string> palindromes = new List<string>();
+foreach (var word in words)
+{
+    bool isPalindrome = IsPalindrome(word);
+    if (isPalindrome)
+    {
+        palindromes.Add(word);
+    }
+}
+var sorted = palindromes
+    .Distinct()
+    .OrderBy(w => w);
+Console.WriteLine(string.Join(", ", sorted));
+
+static bool IsPalindrome(string word)
+{
+    string reversed = new string(word.Reverse().ToArray());
+    return String.Equals(reversed, word);
+}
