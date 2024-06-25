3460d8d3e7a7a85779f88f83cef5aaf3293e7c5b
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-06-25 10:26:57 +0300

    Reverse String Third Solve

---
+++ 19-Strings and Text Processing/Reverse String Third Solve.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string inputText = Console.ReadLine();
+        string reversedString = ReverseString(inputText);
+        Console.WriteLine(reversedString);
+    }
+
+    static string ReverseString(string inputText)
+    {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = inputText.Length - 1; i >= 0; i--)
+        {
+            reversed.Append(inputText[i]);
+        }
+        return reversed.ToString();
+    }
+}
