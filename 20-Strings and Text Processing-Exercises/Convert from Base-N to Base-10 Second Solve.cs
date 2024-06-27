f9bbf3b6c4fd622e9e88cd77738a79f75956258c
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-06-27 16:27:43 +0300

    Convert from Base-N to Base-10 Second Solve

---
+++ 20-Strings and Text Processing-Exercises/Convert from Base-N to Base-10 Second Solve.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        BigInteger convertedNumbers = ConvertedNumbers();
+        PrintResult(convertedNumbers);
+    }
+
+    static void PrintResult(BigInteger convertedNumbers)
+    {
+        Console.WriteLine(convertedNumbers);
+    }
+
+    static BigInteger ConvertedNumbers()
+    {
+        string[] numberAndBase = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        int toBase = int.Parse(numberAndBase[0]);
+        BigInteger number = BigInteger.Parse(numberAndBase[1]);
+        BigInteger sum = 0;
+        int pow = 0;
+        while (number > 0)
+        {
+
+            BigInteger currentNumber = number % 10;
+            BigInteger currentSum = currentNumber * BigInteger.Pow(toBase, pow);
+            sum += currentSum;
+            number /= 10;
+            pow++;
+        }
+        return sum;
+    }
+}
