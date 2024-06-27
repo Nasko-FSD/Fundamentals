d25f794da33790815248a9a3d38f78b738587db1
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-06-27 16:27:10 +0300

    Convert from Base-N to Base-10

---
+++ 20-Strings and Text Processing-Exercises/Convert from Base-N to Base-10.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        BigInteger convertedNumbers = ConvertedNumbers();
+        PrintResult(convertedNumbers);
+    }
+
+    static void PrintResult(BigInteger convertedNumbers)
+    {
+        Console.WriteLine(convertedNumbers);
+    }
+
+    static BigInteger ConvertedNumbers()
+    {
+        string[] numberAndBase = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        int toBase = int.Parse(numberAndBase[0]);
+        string number = numberAndBase[1].ToString();
+        BigInteger sum = 0;
+        int pow = 0;
+        for (int i = number.Length - 1; i >= 0; i--)
+        {
+            char currentChar = number[i];
+            int currentNumber = int.Parse(currentChar.ToString());
+            BigInteger currentSum = currentNumber * BigInteger.Pow(toBase, pow);
+            sum += currentSum;
+            pow++;
+        }
+        return sum;
+    }
+}
