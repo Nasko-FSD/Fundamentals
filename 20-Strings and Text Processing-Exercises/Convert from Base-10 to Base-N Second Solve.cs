34a6b078266d3b852afc9063744b8a0718ff7f1d
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-06-27 14:50:09 +0300

    20-Strings and Text Processing-Exercises

---
+++ 20-Strings and Text Processing-Exercises/Convert from Base-10 to Base-N Second Solve.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        string convertedNumbers = ConvertedNumbers();
+        PrintIntegerToBase(convertedNumbers);
+    }
+
+    static void PrintIntegerToBase(string convertedNumbers)
+    {
+        Console.WriteLine(convertedNumbers);
+    }
+
+    static string ConvertedNumbers()
+    {
+        string[] toBaseAndNumber = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        int toBase = int.Parse(toBaseAndNumber[0]);
+        BigInteger number = BigInteger.Parse(toBaseAndNumber[1]);
+        string result = string.Empty;
+        while (number > 0)
+        {
+            string remainder = (number % toBase).ToString();
+            result = result.Insert(0, remainder);
+            number /= toBase;
+        }
+        return result;
+    }
+}
---
+++ 20-Strings and Text Processing-Exercises/Convert from Base-10 to Base-N.cs
@@ -0,0 +1,44 @@
+﻿using System.Numerics;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string converNumbers = ConvertNumbers();
+        PrintResult(converNumbers);
+    }
+
+    static void PrintResult(string converNumbers)
+    {
+        Console.WriteLine(converNumbers);
+    }
+
+    static string ConvertNumbers()
+    {
+        string[] baseAndNumber = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+        int toBase = int.Parse(baseAndNumber[0]);
+        BigInteger number = BigInteger.Parse(baseAndNumber[1]);
+        StringBuilder converted = new StringBuilder();
+        while (number > 0)
+        {
+            BigInteger remainder = number % toBase;
+            converted.Append(remainder);
+            number /= toBase;
+        }
+        //string reversed = string
+        //    .Concat(converted
+        //    .ToString()
+        //    .Reverse());
+        string reversed = string
+            .Join("", converted
+            .ToString()
+            .Reverse());
+        //for (int i = converted.Length - 1; i >= 0; i--)
+        //{
+        //    reversed += converted[i];
+        //}
+        return reversed;
+    }
+}
