f5a0b2180b0849541bef0b094517f334974a519a
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-06-28 12:12:57 +0300

    Unicode Characters

---
+++ 20-Strings and Text Processing-Exercises/Unicode Characters.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+string inputText = Console.ReadLine();
+StringBuilder sb = new StringBuilder();
+
+foreach (char c in inputText)
+{
+    sb.Append("\\u");
+    sb.Append(String.Format("{0:x4}", (int)c));
+}
+Console.Write(sb);
