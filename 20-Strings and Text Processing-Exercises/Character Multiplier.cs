0b8f3074e5b9be8645e4d3727c880242e2112e7b
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-06-28 14:50:11 +0300

    Character Multiplier

---
+++ 20-Strings and Text Processing-Exercises/Character Multiplier.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int inputString = ReadInput();
+        PrintResult(inputString);
+    }
+
+    static void PrintResult(int inputString)
+    {
+        Console.Write(inputString);
+    }
+
+    static int ReadInput()
+    {
+        string[] inputText = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        string stringOne = inputText[0];
+        string stringTwo = inputText[1];
+        int maxLength = Math.Max(stringOne.Length, stringTwo.Length);
+        int totalSum = 0;
+        for (int i = 0; i < maxLength; i++)
+        {
+            if (i < stringOne.Length && i < stringTwo.Length)
+            {
+                totalSum += stringOne[i] * stringTwo[i];
+            }
+            else if (i < stringOne.Length)
+            {
+                totalSum += stringOne[i];
+            }
+            else
+            {
+                totalSum += stringTwo[i];
+            }
+        }
+        return totalSum;
+    }
+}
