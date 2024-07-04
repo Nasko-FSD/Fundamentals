1446c2c0afa02e8d20e3d15b32a52912af3457ac
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-04 11:40:18 +0300

    Melrah Shake Second Solve

---
+++ 20-Strings and Text Processing-Exercises/Melrah Shake Second Solve.cs
@@ -0,0 +1,17 @@
+﻿string inputText = Console.ReadLine();
+string pattern = Console.ReadLine();
+while (true)
+{
+    int indexFirst = inputText.IndexOf(pattern);
+    int indexSecond = inputText.LastIndexOf(pattern);
+    if (indexFirst == - 1 || indexFirst == indexSecond || pattern == "")
+    {
+        Console.WriteLine("No shake.");
+        break;
+    }
+    inputText = inputText.Remove(indexSecond, pattern.Length);
+    inputText = inputText.Remove(indexFirst, pattern.Length);
+    pattern = pattern.Remove(pattern.Length / 2, 1);
+    Console.WriteLine("Shaked it.");
+}
+Console.WriteLine(inputText);
