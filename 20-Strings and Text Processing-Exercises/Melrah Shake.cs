e4d13f4c77dd2db532fe28397d2bf985bba370ce
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-04 10:43:06 +0300

    Melrah Shake

---
+++ 20-Strings and Text Processing-Exercises/Melrah Shake.cs
@@ -0,0 +1,21 @@
+﻿string word = Console.ReadLine();
+string pattern = Console.ReadLine();
+while (true)
+{
+    int indexFirst = word.IndexOf(pattern);
+    int indexLast = word.LastIndexOf(pattern);
+    if (indexFirst == indexLast)
+    {
+        break;
+    }
+    Console.WriteLine("Shaked it.");
+    word = word.Remove(indexLast, pattern.Length);
+    word = word.Remove(indexFirst, pattern.Length);
+    pattern = pattern.Remove(pattern.Length / 2, 1);
+    if (pattern.Length < 1)
+    {
+        break;
+    }
+}
+Console.WriteLine("No shake.");
+Console.Write($"{word}");
