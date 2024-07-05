77bf0df15828447a6dd77ba675c9d3c555a407a9
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-05 11:27:37 +0300

    Magic Exchageable Words

---
+++ 20-Strings and Text Processing-Exercises/Magic Exchageable Words.cs
@@ -0,0 +1,27 @@
+﻿string[] tokens = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+char[] word1 = tokens[0].ToCharArray();
+char[] word2 = tokens[1].ToCharArray();
+word1 = word1.Distinct().ToArray();
+word2 = word2.Distinct().ToArray();
+
+if (word1.Length != word2.Length)
+{
+    Console.WriteLine("false");
+}
+else
+{
+    Dictionary<char, char> IsMagic = new Dictionary<char, char>();
+    for (int i = 0; i < word1.Length; i++)
+    {
+        if (IsMagic.ContainsKey(word1[i]) == false)
+        {
+            IsMagic[word1[i]] = word2[i];
+        }
+        else if (IsMagic[word1[i]] != word2[i])
+        {
+            Console.WriteLine("false");
+            break;
+        }
+    }
+    Console.WriteLine("true");
+}
