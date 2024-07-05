0aa203a038f733bbf6e72cc135272a33f043f2b0
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-05 09:12:49 +0300

    Melrah Shake Third Solve

---
+++ 20-Strings and Text Processing-Exercises/Melrah Shake Third Solve.cs
@@ -0,0 +1,17 @@
+﻿string inputText = Console.ReadLine();
+string pattern = Console.ReadLine();
+while (true)
+{
+    int firstIndex = inputText.IndexOf(pattern);
+    int lastIndex = inputText.LastIndexOf(pattern);
+    if (firstIndex == lastIndex || firstIndex == -1 || pattern == "")
+    {
+        break;
+    }
+    inputText = inputText.Remove(lastIndex, pattern.Length);
+    inputText = inputText.Remove(firstIndex, pattern.Length);
+    pattern = pattern.Remove(pattern.Length / 2, 1);
+    Console.WriteLine("Shaked it.");
+}
+Console.WriteLine("No shake.");
+Console.WriteLine($"{inputText}");
