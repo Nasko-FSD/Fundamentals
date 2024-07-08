2668ebb58d605029ad2323611d6343ff5bd48330
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-08 15:10:43 +0300

    21-Regular Expressions(RegEx)

---
+++ 21-Regular Expressions(RegEx)/Match Full Name.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+string inputText = Console.ReadLine();
+string regex = @"\b[A-Z][a-z]+\s[A-Z][a-z]+\b";
+MatchCollection matchedNames = Regex.Matches(inputText, regex);
+foreach (Match name in matchedNames)
+{
+    Console.Write(name.Value + " ");
+}
+Console.WriteLine();
