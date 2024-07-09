ecd8e25fad5dc74a31039c89290ff07f31f1e0e2
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-09 21:35:39 +0300

    Match Phone Number

---
+++ 21-Regular Expressions(RegEx)/Match Phone Number.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+string inputLine = Console.ReadLine();
+string pattern = @"(^| )\+359{1}(-| ){1}2{1}(\2)\d{3}(\2)\d{4}\b";
+MatchCollection validNumbers = Regex.Matches(inputLine, pattern);
+var matchedNums = validNumbers
+    .Cast<Match>()
+    .Select(a => a.Value.Trim()).ToArray();
+Console.WriteLine(string.Join(", ", matchedNums));
