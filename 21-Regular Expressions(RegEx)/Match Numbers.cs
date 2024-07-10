3ed87888a9756fd2a46aac8a291f8539adacea08
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-10 16:01:10 +0300

    Match Numbers

---
+++ 21-Regular Expressions(RegEx)/Match Numbers.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+string inputLine = Console.ReadLine();
+string pattern = @"(^|(?<=\s))-?\d+(.\d+)?($|(?=\s))";
+MatchCollection validNumbers = Regex.Matches(inputLine, pattern);
+foreach (Match number in validNumbers)
+{
+    Console.Write(number.Value + " ");
+}
