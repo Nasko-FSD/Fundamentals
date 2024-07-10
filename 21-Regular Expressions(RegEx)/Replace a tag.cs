e59cec4f1dcd8c00c21d1704cefe4040d66b25a8
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-10 17:13:29 +0300

    Replace a tag

---
+++ 21-Regular Expressions(RegEx)/Replace a tag.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+while (true)
+{
+    string inputLine = Console.ReadLine();
+    if (inputLine == "end")
+    {
+        break;
+    }
+    string pattern = @"<a.*?href.*?=(.*)>(.*?)<\/a>";
+    string replacement = @"[URL href=$1]$2[/URL]";
+
+    inputLine = Regex.Replace(inputLine, pattern, replacement);
+    Console.WriteLine(inputLine);
+}
