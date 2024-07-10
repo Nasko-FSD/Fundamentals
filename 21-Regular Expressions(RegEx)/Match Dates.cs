b359d265d36fe663e819f970dd84f77dd0de6dc0
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-10 15:27:12 +0300

    Match Dates

---
+++ 21-Regular Expressions(RegEx)/Match Dates.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+string inputLine = Console.ReadLine();
+string regex = @"\b(\d{2})([-|.|/])([A-Z][a-z]{2})(\2)(\d{4})\b";
+MatchCollection dates = Regex.Matches(inputLine, regex);
+foreach (Match date in dates)
+{
+    var day = date.Groups[1].Value;
+    var month = date.Groups[3].Value;
+    var year = date.Groups[5].Value;
+
+    Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+}
