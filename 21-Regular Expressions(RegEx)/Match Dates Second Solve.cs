9c065be9e7b68bb16ca717d77f1f35c78fd7a30f
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-10 16:03:16 +0300

    Match Dates Second Solve

---
+++ 21-Regular Expressions(RegEx)/Match Dates Second Solve.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+string inputLine = Console.ReadLine();
+string regex = @"\b(?<day>\d{2})(-|\.|/)(?<month>[A-Z][a-z]{2})(\1)(?<year>\d{4})\b";
+MatchCollection dates = Regex.Matches(inputLine, regex);
+foreach (Match date in dates)
+{
+    var day = date.Groups["day"].Value;
+    var month = date.Groups["month"].Value;
+    var year = date.Groups["year"].Value;
+
+    Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+}
