223737483d7576714aa3f7569fd421d73d1cef79
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-10 10:50:10 +0300

    Match Hexadecimal Numbers Second Solve

---
+++ 21-Regular Expressions(RegEx)/Match Hexadecimal Numbers Second Solve.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+string inputLine = Console.ReadLine();
+string pattern = @"\b(?:0x)?[0-9A-F]+\b";
+MatchCollection validHexa = Regex.Matches(inputLine, pattern);
+var hexaDecimals = Regex.Matches(inputLine, pattern)
+    .Cast<Match>()
+    .Select(m => m.Value)
+    .ToArray();
+Console.WriteLine(string.Join(" ", hexaDecimals));
