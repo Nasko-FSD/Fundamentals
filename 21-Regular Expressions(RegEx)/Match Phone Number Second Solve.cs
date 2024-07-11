dc2e3dfbe455d241e74480f094783a1fafde6929
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-11 07:34:55 +0300

    Match Phone Number Second Solve

---
+++ 21-Regular Expressions(RegEx)/Match Phone Number Second Solve.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+string inputLine = Console.ReadLine();
+string pattern = @"(^| )(\+359)?(-| )?2?(\3)\d{3}(\3)\d{4}\b";
+MatchCollection validNumbers = Regex.Matches(inputLine, pattern);
+var matchedNums = validNumbers
+    .Cast<Match>()
+    .Select(a => a.Value.Trim()).ToArray();
+Console.WriteLine(string.Join(", ", matchedNums));
