eb8edd0054b5088355d95807aeded5388360933d
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-11 11:07:28 +0300

    Extract Sentences by Keyword

---
+++ 22-Regular Expressions (RegEx) - Exercises/Extract Sentences by Keyword.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+string searchingWord = Console.ReadLine();
+string[] inputLine = Console.ReadLine()
+    .Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+string pattern = $@"\W{searchingWord}\W";
+var validSentences = inputLine
+    .Where(sentence => Regex.IsMatch(sentence, pattern))
+    .Select(sentence => sentence.Trim());
+foreach (var sentence in validSentences)
+{
+    Console.WriteLine(sentence);
+}
