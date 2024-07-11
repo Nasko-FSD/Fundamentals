8e1f505bc61637c4c0f7db7ea3b4fa3234a41f84
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-11 23:03:31 +0300

    Camera View

---
+++ 22-Regular Expressions (RegEx) - Exercises/Camera View.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+var numbers = Regex
+    .Split(Console.ReadLine(), "\\s+")
+    .Select(x => int.Parse(x))
+    .ToArray();    
+string inputLine = Console.ReadLine();
+string pattern = $@"\|<.{{{numbers[0]}}}([^|]{{0,{numbers[1]}}})";
+Regex regex = new Regex(pattern);
+MatchCollection cameraNumbers = regex.Matches(inputLine);
+List<string> result = new List<string>();
+foreach (Match cameraNumber in cameraNumbers)
+{
+    result.Add(cameraNumber.Groups[1].Value);
+}
+Console.WriteLine(string.Join(", ", result)); 
