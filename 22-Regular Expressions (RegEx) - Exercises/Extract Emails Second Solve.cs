95657ac885836e42e75a33be27537134dc9c29d8
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-19 10:37:25 +0300

    Extract Emails Second Solve

---
+++ 22-Regular Expressions (RegEx) - Exercises/Extract Emails Second Solve.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+string inputLine = Console.ReadLine();
+string patter = @"(^|(?<=\s))([a-zA-Z0-9]+)(.|-|_)?([A-Za-z0-9]+)?(@)([a-zA-Z]+([.|-][A-Za-z]+)+)(\b|(?=\s))";
+MatchCollection validEmails = Regex.Matches(inputLine, patter);
+foreach (Match mail in validEmails)
+{
+    Console.WriteLine(mail);
+}
