30e79545cf0f62d6b59f2ccb718ea48dfc9e8d21
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-22 19:23:18 +0300

    Key Replacer

---
+++ 22-Regular Expressions (RegEx) - Exercises/Key Replacer.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+string keys = Console.ReadLine();
+string pattern = @"(^([A-Za-z]+)(?=[<|\\|\|]))|((?<=[<|\\|\|])([A-Za-z]+)$)";
+MatchCollection allKeys = Regex.Matches(keys, pattern);
+string firstKey = allKeys[0].Value;
+string secondKey = allKeys[1].Value;
+string textInput = Console.ReadLine();
+string secondPattern = $@"(?<={firstKey})(.*?)(?={secondKey})";
+MatchCollection validText = Regex.Matches(textInput, secondPattern);
+bool foundValidText = false;
+foreach (Match match in validText)
+{
+    if (match.Length > 1)
+    {
+        Console.Write(string.Join("", match.Groups[1].Value));
+        foundValidText = true;
+    }
+}
+if (foundValidText == false)
+{
+    Console.WriteLine("Empty result");
+}
