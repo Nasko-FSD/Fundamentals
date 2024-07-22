2334beed21daffb69894cb67c3b695e74a81218f
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-22 19:27:16 +0300

    Key Replacer Second Solve

---
+++ 22-Regular Expressions (RegEx) - Exercises/Key Replacer Second Solve.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+string keys = Console.ReadLine();
+string pattern = @"(^([A-Za-z]+)(?=[<|\\|\|]))|((?<=[<|\\|\|])([A-Za-z]+)$)";
+MatchCollection allKeys = Regex.Matches(keys, pattern);
+string firstKey = allKeys[0].Value;
+string secondKey = allKeys[1].Value;
+string textInput = Console.ReadLine();
+string secondPattern = $@"{firstKey}(?!{secondKey})(.*?){secondKey}";
+MatchCollection validText = Regex.Matches(textInput, secondPattern);
+if (validText.Count > 0)
+{
+    foreach (Match match in validText)
+    {
+        if (match.Length > 1)
+        {
+            Console.Write(string.Join("", match.Groups[1].Value));
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Empty result");
+}
