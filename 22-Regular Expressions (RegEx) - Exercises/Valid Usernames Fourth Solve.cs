e5a5c9fa7b8d3a92c427497f849280f7f1df295c
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-23 15:16:53 +0300

    Valid Usernames Fourth Solve

---
+++ 22-Regular Expressions (RegEx) - Exercises/Valid Usernames Fourth Solve.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+string text = Console.ReadLine();
+string pattern = @"\b[a-zA-Z]\w{2,24}\b";
+Regex regex = new Regex(pattern);
+MatchCollection matchCollection = regex.Matches(text);
+int sum = 0;
+int maxSum = 0;
+string first = "";
+string second = "";
+for (int i = 0; i < matchCollection.Count - 1; i++)
+{
+    sum = matchCollection[i].Length + matchCollection[i + 1].Length;
+
+    if (sum > maxSum)
+    {
+        maxSum = sum;
+        first = matchCollection[i].ToString();
+        second = matchCollection[i + 1].ToString();
+    }
+}
+Console.WriteLine(first);
+Console.WriteLine(second);
