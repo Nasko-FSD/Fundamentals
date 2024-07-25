a166d4f8c4fdf756286e1e90cdc49ca9bd4bb1ff
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-07-25 21:00:57 +0300

    Camera View Second Solve

---
+++ 22-Regular Expressions (RegEx) - Exercises/Camera View Second Solve.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+Regex regex = new Regex(@"\|<(?<picture>\w+)");
+int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int skipNumber = numbers[0];
+int takeNumber = numbers[1];
+
+string inputLine = Console.ReadLine();
+MatchCollection matches = regex.Matches(inputLine);
+
+string[] allResults = new string[matches.Count];
+int index = 0;
+foreach (Match match in matches)
+{
+    string currentMatch = match.Groups["picture"].Value;
+
+    char[] resultChars = currentMatch.Skip(skipNumber)
+        .Take(takeNumber)
+        .ToArray();
+
+    string currentRresult = string.Join("", resultChars);
+    allResults[index++] = currentRresult;
+}
+Console.WriteLine(string.Join(", ", allResults));
