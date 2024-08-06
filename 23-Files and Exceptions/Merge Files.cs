0af926cea1540d31ceb752d0b30e16b3d5dc5c2b
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-08-06 12:21:57 +0300

    Merge Files

---
+++ 23-Files and Exceptions/Merge Files.cs
@@ -0,0 +1,21 @@
+﻿string[] fileOnePath = File.ReadAllLines("FileOne.txt");
+string[] fileTwoPath = File.ReadAllLines("FileTwo.txt");
+
+File.WriteAllText("Output.txt", string.Empty);
+
+int maxLength = Math.Max(fileOnePath.Length, fileTwoPath.Length);
+
+using (StreamWriter writer = new StreamWriter("Output.txt", append: true))
+{
+    for (int i = 0; i < maxLength; i++)
+    {
+        if (i < fileOnePath.Length)
+        {
+            writer.WriteLine(fileOnePath[i]);
+        }
+        if (i < fileTwoPath.Length)
+        {
+            writer.WriteLine(fileTwoPath[i]);
+        }
+    }
+}
