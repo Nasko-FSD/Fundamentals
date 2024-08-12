530a94ad037b4a02930af87bb974fbe4cc835958
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2024-08-12 23:08:58 +0300

    Ladybugs Second Solve

---
+++ 23-Files and Exceptions/Ladybugs Second Solve.cs
@@ -0,0 +1,83 @@
+﻿int fieldSize = int.Parse(Console.ReadLine());
+
+if (fieldSize == 0)
+{
+    return;
+}
+
+int[] fieldValues = new int[fieldSize];
+int[] initialBugs = Console.ReadLine()
+    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+string[] commands = Console.ReadLine()
+    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+foreach (var index in initialBugs)
+{
+    if (index >= 0 &&
+        index < fieldSize)
+    {
+        fieldValues[index] = 1;
+    }
+}
+while (true)
+{
+    if (commands[0] == "end")
+    {
+        break;
+    }
+
+    int fieldKey = int.Parse(commands[0]);
+    string direction = commands[1];
+    int fieldValue = int.Parse(commands[2]);
+
+    if (fieldKey < 0 ||
+        fieldKey >= fieldSize ||
+        fieldValues[fieldKey] == 0)
+    {
+        commands = Console.ReadLine()
+        .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        continue;
+    }
+
+    fieldValues[fieldKey] = 0;
+
+    if (direction == "right")
+    {
+        fieldKey += fieldValue;
+    }
+
+    else if (direction == "left")
+    {
+        fieldKey -= fieldValue;
+    }
+
+    while (fieldKey >= 0 &&
+           fieldKey < fieldSize &&
+           fieldValues[fieldKey] == 1)
+    {
+        if (direction == "right")
+        {
+            fieldKey += fieldValue;
+        }
+
+        else if (direction == "left")
+        {
+            fieldKey -= fieldValue;
+        }
+    }
+
+    if (fieldKey >= 0 &&    
+        fieldKey < fieldSize)
+    {
+        fieldValues[fieldKey] = 1;
+    }
+
+    commands = Console.ReadLine()
+        .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+}
+foreach (var index in fieldValues)
+{
+    Console.Write(index + " ");
+}
