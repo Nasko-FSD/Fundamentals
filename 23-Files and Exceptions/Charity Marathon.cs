556797b04bbdda095e1315f177a27662574589f7
Nasko-FSD <atanasnw@gmail.com>
2024-08-13 16:39:56 +0300

    Charity Marathon

---
+++ 23-Files and Exceptions/Charity Marathon.cs
@@ -0,0 +1,18 @@
+﻿long days = long.Parse(Console.ReadLine());
+long runners = long.Parse(Console.ReadLine());
+long laps = long.Parse(Console.ReadLine());
+long trackLength = long.Parse(Console.ReadLine());
+long initialCapacity = long.Parse(Console.ReadLine());
+decimal money = decimal.Parse(Console.ReadLine());
+
+long totalCapacity = initialCapacity * days;
+
+if (runners > totalCapacity)
+{
+    runners = totalCapacity;
+}
+
+long totalDistance = runners * laps * trackLength;
+long kilometers = totalDistance / 1000;
+decimal totalMoney = money * kilometers;
+Console.WriteLine($"Money raised: {totalMoney:f2}");
