6df2142eab2431bf61da8ce8d2e08ac874465964
Nasko-FSD <atanasnw@gmail.com>
2024-08-14 14:40:02 +0300

    Poke Mon

---
+++ 23-Files and Exceptions/Poke Mon Second Solve.cs
@@ -0,0 +1,17 @@
+﻿int power = int.Parse(Console.ReadLine());
+int distance = int.Parse(Console.ReadLine());
+int exhaustion = int.Parse(Console.ReadLine());
+int counter = 0;
+double fiftyPercent = power * 0.5;
+while (power >= distance)
+{
+    power -= distance;
+    counter++;
+    if (power == fiftyPercent &&
+        exhaustion > 0)
+    {
+        power /= exhaustion;
+    }
+}
+Console.WriteLine($"{power}");
+Console.Write($"{counter}");
---
+++ 23-Files and Exceptions/Poke Mon.cs
@@ -0,0 +1,23 @@
+﻿int power = int.Parse(Console.ReadLine());
+int distance = int.Parse(Console.ReadLine());
+int exhaustion = int.Parse(Console.ReadLine());
+int counter = 0;
+double fiftyPercent = power * 0.5;
+while (power >= distance)
+{
+    power -= distance;
+    counter++;
+    if (power == fiftyPercent)
+    {
+        try
+        {
+            power /= exhaustion;
+        }
+        catch (Exception)
+        {
+            continue;
+        }
+    }
+}
+Console.WriteLine($"{power}");
+Console.Write($"{counter}");
