6e0d9b549756b7dc8cab31264b0ae31566d284ec
Nasko-FSD <atanasnw@gmail.com>
2024-08-15 15:46:51 +0300

    Pokemon Don't Go Second Solve

---
+++ 23-Files and Exceptions/Pokemon Don't Go Second Solve.cs
@@ -0,0 +1,42 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+long sum = 0;
+ 
+while (numbers.Count > 0)
+{
+    int currentIndex = int.Parse(Console.ReadLine());
+    int removedNumber = 0;
+
+    if (currentIndex < 0)
+    {
+        removedNumber = numbers[0];
+        numbers[0] = numbers[numbers.Count - 1];
+    }
+    else if (currentIndex > numbers.Count - 1)  
+    {
+        removedNumber = numbers[numbers.Count - 1];
+        numbers[numbers.Count - 1] = numbers[0];
+    }
+    else
+    {
+        removedNumber = numbers[currentIndex];
+        numbers.RemoveAt(currentIndex);
+    }
+    sum += removedNumber;
+
+    for (int i = 0; i < numbers.Count; i++)
+    {
+        int currentNumber = numbers[i];
+        if (currentNumber <= removedNumber)
+        {
+            numbers[i] += removedNumber;
+        }
+        else
+        {
+            numbers[i] -= removedNumber;
+        }
+    }
+}
+Console.WriteLine(sum);
