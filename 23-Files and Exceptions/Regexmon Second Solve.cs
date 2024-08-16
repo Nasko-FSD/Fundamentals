cc077a8ccc446090b416af62092917d0318d0343
Nasko-FSD <atanasnw@gmail.com>
2024-08-16 11:55:12 +0300

    Regexmon Second Solve

---
+++ 23-Files and Exceptions/Regexmon Second Solve.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+Regex bojoRegex = new Regex(@"[a-zA-Z]+\-[a-zA-Z]+");
+Regex didiRegex = new Regex(@"[^a-zA-Z\-]+");
+
+string text = Console.ReadLine();
+
+while (true)
+{
+
+    Match didiMatch = didiRegex.Match(text);
+    if (didiMatch.Success == false)
+    {
+        break;
+    }
+    int didiIndex = didiMatch.Index + didiMatch.Length;
+    text = text.Substring(didiIndex);
+    Console.WriteLine(didiMatch.Value);
+
+    Match bojoMatch = bojoRegex.Match(text);
+    if (bojoMatch.Success == false)
+    {
+        break;
+    }
+    int bojoIndex = bojoMatch.Index + bojoMatch.Length;
+    text = text.Substring(bojoIndex);
+    Console.WriteLine(bojoMatch.Value);
+}
