9c6ddb95fec4aac9c4e84b0d838f3f24d23c6a1b
Nasko-FSD <atanasnw@gmail.com>
2024-08-16 12:13:07 +0300

    Regexmon Third Solve

---
+++ 23-Files and Exceptions/Regexmon Third Solve.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+Regex bojoRegex = new Regex(@"[a-zA-Z]+\-[a-zA-Z]+");
+Regex didiRegex = new Regex(@"[^a-zA-Z\-]+");
+
+string text = Console.ReadLine();
+
+int index = 0;
+while (true)
+{
+    Match didiMatch = didiRegex.Match(text, index);
+
+    if (didiMatch.Success == false)
+    {
+        break;
+    }
+
+    index = didiMatch.Index + didiMatch.Length;
+    Console.WriteLine(didiMatch.Value, index);
+
+    Match bojoMatch = bojoRegex.Match(text, index);
+
+    if (bojoMatch.Success == false)
+    {
+        break;
+    }
+
+    index = bojoMatch.Index + bojoMatch.Length;
+    Console.WriteLine(bojoMatch.Value);
+}
