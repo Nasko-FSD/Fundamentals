8f4fe248aa8888d7bd1ef864b4e1b8ed61176e5e
Nasko-FSD <atanasnw@gmail.com>
2024-08-21 14:34:48 +0300

    Memory View

---
+++ 24-Exam Preparation 1/Memory View.cs
@@ -0,0 +1,43 @@
+﻿string inputLine = Console.ReadLine();
+string allInput = "";
+while (inputLine != "Visual Studio crash")
+{
+    allInput += inputLine + " ";
+    inputLine = Console.ReadLine();
+}
+
+int[] tokens = allInput
+    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int[] length = new int[1];
+int[] allNumbers = new int[1];
+int startIndex = 0;
+for (int i = 0; i < tokens.Length; i++)
+{
+    if (tokens[i] == 32656)
+    {
+        length = tokens
+           .Skip(i + 4)
+           .Take(1)
+           .ToArray();
+
+        allNumbers = new int[length[0]];
+        startIndex = i;
+    }
+
+    else
+    {
+        continue;
+    }
+
+    for (int j = 1; j <= allNumbers.Length; j++)
+    {
+        int currentChar = tokens[startIndex + 6];
+        char letter = (char)currentChar;
+        Console.Write(letter);
+        startIndex++;
+    }
+    Console.WriteLine();
+}
