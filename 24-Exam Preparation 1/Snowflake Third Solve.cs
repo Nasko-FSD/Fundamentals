aa2703801006940f2445dc8e2a0765d87f93e2ec
Nasko-FSD <atanasnw@gmail.com>
2024-08-29 13:57:56 +0300

    Snowflake Third Solve

---
+++ 24-Exam Preparation 1/Snowflake Third Solve.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+string lines = string.Empty;
+string pattern = "^([^A-Za-z0-9]+)\n([0-9_]+)\n([^A-Za-z0-9]+)([0-9_]+)(?<core>[A-Za-z]+)([0-9_]+)([^A-Za-z0-9]+)\n([0-9_]+)\n([^A-Za-z0-9]+)\n$";
+
+for (int i = 1; i <= 5; i++)
+{
+    lines += Console.ReadLine() + "\n";
+}
+Regex regex = new Regex(pattern);
+if (regex.IsMatch(lines))
+{
+    int coreLength = regex.Match(lines).Groups["core"].Value.Length;
+    Console.WriteLine("Valid");
+    Console.WriteLine(coreLength);
+}
+else
+{
+    Console.WriteLine("Invalid");
+}
