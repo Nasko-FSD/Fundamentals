6817c54f212fccc233bd3858524042f3c1a5be2d
Nasko-FSD <atanasnw@gmail.com>
2024-08-29 13:56:22 +0300

    Snowflake

---
+++ 24-Exam Preparation 1/Snowflake.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+string surfacePattern = @"[^a-zA-Z0-9]+";
+string mantlePattern = @"[_\d]+";
+string corePattern = @"[a-zA-Z]+";
+
+bool isFine = true;
+int coreLength = -1;
+string currentPattern = "";
+for (int i = 0; i < 5; i++)
+{
+    string inputLIne = Console.ReadLine();
+    if (i == 0 || i == 4)
+    {
+        currentPattern = surfacePattern;
+    }
+    else if (i == 1 || i == 3)
+    {
+        currentPattern = mantlePattern;
+    }
+    else
+    {
+        currentPattern = corePattern;
+    }
+
+    Regex regex = new Regex(currentPattern);
+
+    if (regex.IsMatch(inputLIne) && i == 2)
+    {
+        coreLength = regex.Match(inputLIne).Length;
+    }
+    if (regex.IsMatch(inputLIne) == false)
+    {
+        isFine = false;
+    }
+}
+if (isFine)
+{
+    Console.WriteLine("Valid");
+    Console.WriteLine($"{coreLength}");
+}
+else
+{
+    Console.WriteLine("Invalid");
+}
