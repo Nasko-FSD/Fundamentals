cc45a8e40c78ea519e348793664648253ce6a1e8
Nasko-FSD <atanasnw@gmail.com>
2024-08-30 15:56:41 +0300

    Padawan Equipment Third Solve.cs

---
+++ 25-Exam Preparation 2/Padawan Equipment Third Solve.cs
@@ -0,0 +1,19 @@
+﻿double money = double.Parse(Console.ReadLine());
+int studentsCount = int.Parse(Console.ReadLine());
+double lightSaber = double.Parse(Console.ReadLine());
+double robesPrice = double.Parse(Console.ReadLine());
+double beltsPrice = double.Parse(Console.ReadLine());
+
+double sabersTotalPrice = lightSaber * Math.Ceiling(studentsCount * 1.1);
+double beltsTotalPrice = beltsPrice * (studentsCount - studentsCount / 6);
+double robesTotalPrice = robesPrice * studentsCount;
+double equipmentPrice = robesTotalPrice + beltsTotalPrice + sabersTotalPrice;
+if (money >= equipmentPrice)
+{
+    Console.WriteLine($"The money is enough - it would cost {equipmentPrice:f2}lv.");
+}
+else
+{
+    double requiredMoney = equipmentPrice - money;
+    Console.WriteLine($"John will need {requiredMoney:f2}lv more.");
+}
