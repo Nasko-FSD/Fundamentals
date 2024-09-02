8e896e9fc3d710010c5156f0ff9912ef8bcceee6
Nasko-FSD <atanasnw@gmail.com>
2024-09-02 18:36:18 +0300

    Kamino Factory

---
+++ 25-Exam Preparation 2/Kamino Factory.cs
@@ -0,0 +1,56 @@
+﻿int dnaLength = int.Parse(Console.ReadLine());
+int bestLength = 0;
+int bestSum = 0;
+int bestStartingIndex = int.MaxValue;
+int bestSampleIndex = 0;
+int[] bestSequence = new int[dnaLength];
+int sampleIndex = 0;
+
+string sequence;
+while ((sequence = Console.ReadLine()) != "Clone them!")
+{
+    sampleIndex++;
+
+    int[] currentSequence = sequence
+        .Split(new[] { '!' }, StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    int currentLength = 0;
+    int maxCurrentLength = 0;
+    int currentStartingIndex = 0;
+    int currentSum = currentSequence.Sum();
+
+    for (int i = 0; i < currentSequence.Length; i++)
+    {
+        if (currentSequence[i] == 1)
+        {
+            currentLength++;
+
+            if (currentLength > maxCurrentLength)
+            {
+                maxCurrentLength = currentLength;
+                currentStartingIndex = i - currentLength + 1;
+            }
+        }
+
+        else
+        {
+            currentLength = 0;
+        }
+    }
+
+    if (maxCurrentLength > bestLength ||
+        (maxCurrentLength == bestLength && currentStartingIndex < bestStartingIndex) ||
+        (maxCurrentLength == bestLength && currentStartingIndex == bestStartingIndex && currentSum > bestSum))
+    {
+        bestLength = maxCurrentLength;
+        bestStartingIndex = currentStartingIndex;
+        bestSum = currentSum;
+        bestSequence = currentSequence;
+        bestSampleIndex = sampleIndex;
+    }
+}
+
+Console.WriteLine($"Best DNA sample {bestSampleIndex} with sum: {bestSum}.");
+Console.WriteLine(string.Join(" ", bestSequence));
