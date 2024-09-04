00fa82642510c4c413a6d2d7c077dde68370eaf2
Nasko-FSD <atanasnw@gmail.com>
2024-09-04 21:09:47 +0300

    Anonymous Threat Second Solve

---
+++ 25-Exam Preparation 2/Anonymous Threat Second Solve.cs
@@ -0,0 +1,70 @@
+﻿List<string> words = Console.ReadLine()
+    .Split(" ".ToCharArray())
+    .ToList();
+
+string input = "";
+
+while ((input = Console.ReadLine()) != "3:1")
+{
+
+    string[] tokens = input
+    .Split(" ".ToCharArray())
+    .ToArray();
+
+    string command = tokens[0];
+
+    if (command == "merge")
+    {
+        int startIndex = int.Parse(tokens[1]);
+        int endIndex = int.Parse(tokens[2]);
+
+        if (endIndex < 0 || startIndex > words.Count - 1)
+        {
+            continue;
+        }
+
+        if (startIndex < 0)
+        {
+            startIndex = 0;
+        }
+
+        if (endIndex >= words.Count)
+        {
+            endIndex = words.Count - 1;
+        }
+
+        string concatWord = "";
+        for (int i = startIndex; i <= endIndex; i++)
+        {
+            concatWord += words[startIndex];
+            words.RemoveAt(startIndex);
+        }
+        words.Insert(startIndex, concatWord);
+    }
+
+    if (command == "divide")
+    {
+
+        int index = int.Parse(tokens[1]);
+        int partitions = int.Parse(tokens[2]);
+        string element = words[index];
+
+        int partLength = element.Length / partitions;
+
+        List<string> newWords = new List<string>();
+
+        for (int i = 0; i < partitions; i++)
+        {
+            if (partitions - i == 1)
+            {
+                newWords.Add(element.Substring(i * partLength));
+                break;
+            }
+            string word = element.Substring(i * partLength, partLength);
+            newWords.Add(word);
+        }
+        words.RemoveAt(index);
+        words.InsertRange(index, newWords);
+    }
+}
+Console.WriteLine(string.Join(" ", words));
