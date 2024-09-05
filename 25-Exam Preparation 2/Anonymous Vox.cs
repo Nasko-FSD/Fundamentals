5d6004ce4b02ad1c81d09e3db4b81216638043b4
Nasko-FSD <atanasnw@gmail.com>
2024-09-05 18:40:13 +0300

    Anonymous Vox.

---
+++ 25-Exam Preparation 2/Anonymous Vox.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+string inputText = Console.ReadLine();
+string placeHolderPattern = @"(?<start>[a-zA-Z]+)(?<placeHolder>.+)(\k<start>)";
+
+List<string> values = Console.ReadLine()
+    .Split(new char[] { '{', '}' }, StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+MatchCollection matches = Regex.Matches(inputText, placeHolderPattern);
+
+foreach (Match matched in matches)
+{
+    if (values.Count > 0)
+    {
+        string currentMatch = matched.ToString();
+
+        //string onlyPlaceHolder = matched.Groups["placeHolder"].Value.ToString(); 
+
+        string newBlock = matched.Groups["start"].Value + values[0] + matched.Groups["start"].Value;
+
+        inputText = inputText.Replace(currentMatch, newBlock);
+
+        values.RemoveAt(0);
+    }
+    else
+    {
+        break;
+    }
+}
+Console.WriteLine(inputText);
