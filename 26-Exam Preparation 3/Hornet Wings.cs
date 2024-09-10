feae6245f7112557c17c2a10c619ea701c558412
Nasko-FSD <atanasnw@gmail.com>
2024-09-10 12:11:43 +0300

    Hornet Wings

---
+++ 26-Exam Preparation 3/Hornet Wings.cs
@@ -0,0 +1,12 @@
+﻿int flaps = int.Parse(Console.ReadLine());
+double distance = double.Parse(Console.ReadLine());
+int endurance =  int.Parse(Console.ReadLine()); 
+
+
+double calcDistance = (flaps / 1000) * distance;
+
+
+double flyLength = (flaps / 100) + (flaps / endurance) * 5;
+
+Console.WriteLine($"{calcDistance:f2} m.");
+Console.WriteLine($"{flyLength} s.");
