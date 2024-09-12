ba916999c3197be050bd9d7141dbc60c5a381775
Nasko-FSD <atanasnw@gmail.com>
2024-09-12 16:24:44 +0300

    Hornet Armada Second Solve

---
+++ 26-Exam Preparation 3/Hornet Armada Second Solve.cs
@@ -0,0 +1,64 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, long> legionsActivity = new Dictionary<string, long>();
+Dictionary<string, Dictionary<string, long>> legionInfo = new Dictionary<string, Dictionary<string, long>>();
+
+while (n-- > 0)
+{
+    string[] data = Console.ReadLine()
+        .Split("=->: "
+        .ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+    long lastActivity = long.Parse(data[0]);
+    string legionName = data[1];
+    string soldierType = data[2];
+    long soldierCount = long.Parse(data[3]);
+
+    if (legionInfo.ContainsKey(legionName) == false)
+    {
+        legionInfo.Add(legionName, new Dictionary<string, long>());
+        legionsActivity.Add(legionName, lastActivity);
+
+    }
+    if (legionInfo[legionName].ContainsKey(soldierType) == false)
+    {
+        legionInfo[legionName].Add(soldierType, soldierCount);
+    }
+    else
+    {
+        legionInfo[legionName][soldierType] += soldierCount;
+    }
+    if (legionsActivity[legionName] < lastActivity)
+    {
+        legionsActivity[legionName] = lastActivity;
+    }
+}
+
+string command = Console.ReadLine();
+
+if (command.IndexOf('\\') != -1)
+{
+    int activity = int.Parse(command.Substring(0, command.IndexOf('\\')));
+    string soldierType = command.Substring(command.IndexOf("\\") + 1);
+
+    foreach (var legion in legionInfo
+        .Where(e => legionInfo[e.Key].ContainsKey(soldierType))
+        .OrderByDescending(e => e.Value[soldierType]))
+    {
+        if (legionsActivity[legion.Key] < activity)
+        {
+            Console.WriteLine($"{legion.Key} -> {legion.Value[soldierType]}");
+        }
+    }
+}
+else
+{
+    foreach (var item in legionsActivity
+        .OrderByDescending(x => x.Value))
+    {
+        if (legionInfo[item.Key].ContainsKey(command))
+        {
+            Console.WriteLine($"{item.Value} : {item.Key}");
+        }
+    }
+}
