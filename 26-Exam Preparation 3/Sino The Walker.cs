0e8dcd1c54bd9c98d1fbb8ed26a4c3e9797948c6
Nasko-FSD <atanasnw@gmail.com>
2024-09-16 11:55:47 +0300

    Sino The Walker

---
+++ 26-Exam Preparation 3/Sino The Walker.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+var leaves = DateTime.ParseExact(Console.ReadLine()
+    ,"HH:mm:ss", CultureInfo.InvariantCulture);
+long steps = long.Parse(Console.ReadLine()) % 86400;
+long timeS = long.Parse(Console.ReadLine()) % 86400;
+
+long walkingTime = steps * timeS;
+
+DateTime walking = leaves.AddSeconds(walkingTime);
+
+Console.WriteLine($"Time Arrival: {walking:HH:mm:ss}");
