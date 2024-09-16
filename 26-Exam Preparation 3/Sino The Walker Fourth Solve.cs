336ae69b68d20d0d84093deb1237b2526345eacc
Nasko-FSD <atanasnw@gmail.com>
2024-09-16 11:58:11 +0300

    Sino The Walker Fourth Solve

---
+++ 26-Exam Preparation 3/Sino The Walker Fourth Solve.cs
@@ -0,0 +1,9 @@
+﻿string input = Console.ReadLine();
+int stepsCount = int.Parse(Console.ReadLine()) % 86400;
+int secondsPerStep = int.Parse(Console.ReadLine()) % 86400;
+
+DateTime d = DateTime.Parse(input);
+TimeSpan duration = TimeSpan.FromSeconds(secondsPerStep * stepsCount);
+
+TimeSpan ts = d.TimeOfDay.Add(duration);
+Console.WriteLine("Time Arrival: {0:D2}:{1:D2}:{2:D2}", ts.Hours, ts.Minutes, ts.Seconds);
