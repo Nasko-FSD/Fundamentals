fa008c3096f811d795ba6759c0bf0b6368a3e83c
Nasko-FSD <atanasnw@gmail.com>
2024-09-16 11:57:43 +0300

    Sino The Walker Third Solve

---
+++ 26-Exam Preparation 3/Sino The Walker Third Solve.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+TimeSpan time = TimeSpan.ParseExact(Console.ReadLine(), @"hh\:mm\:ss", CultureInfo.InvariantCulture);
+int stepsCount = int.Parse(Console.ReadLine()) % 86400;
+int secondsPerSteps = int.Parse(Console.ReadLine()) % 86400;
+
+double timeToAdd = stepsCount * secondsPerSteps;
+
+time += TimeSpan.FromSeconds(timeToAdd);
+
+Console.WriteLine($"Time Arrival: {time.ToString(@"hh\:mm\:ss")}");
