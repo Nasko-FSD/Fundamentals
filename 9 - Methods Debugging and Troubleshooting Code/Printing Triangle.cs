1504b427fe37985b4612a74945c2a4129be3b7f8
Nasko-FSD <atanasnw@gmail.com>
2024-09-19 00:20:05 +0300

    9 - Methods Debugging and Troubleshooting Code

---
+++ 9 - Methods Debugging and Troubleshooting Code/Printing Triangle.cs
@@ -0,0 +1,24 @@
+﻿PrintingTriangle();
+static void PrintingTriangle()
+{
+    int number = int.Parse(Console.ReadLine());
+    var count = 1;
+    for (int UpperRows = 1; UpperRows <= number; UpperRows++)
+    {
+        for (int col = 1; col <= count; col++)
+        {
+            Console.Write(col + " ");
+        }
+        Console.WriteLine();
+        count++;
+    }
+    for (int UpperRows = 1; UpperRows <= number; UpperRows++)
+    {
+        for (int col = 1; col <= count - 2; col++)
+        {
+            Console.Write(col + " ");
+        }
+        Console.WriteLine();
+        count--;
+    }
+}
