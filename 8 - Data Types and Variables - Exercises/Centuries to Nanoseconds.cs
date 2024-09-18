747c02210ad9ef7efe400f4c74231b0fcf19a719
Nasko-FSD <atanasnw@gmail.com>
2024-09-19 00:19:47 +0300

    8 - Data Types and Variables - Exercises

---
+++ 8 - Data Types and Variables - Exercises/Centuries to Nanoseconds.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Centuries = ");
+int centuries = int.Parse(Console.ReadLine());
+int years = centuries * 100;
+int days = (int)(years * 365.2422);
+ulong hours = (ulong)days * 24;
+ulong minutes = hours * 60;
+ulong seconds = minutes * 60;
+ulong milliseconds = seconds * 1000;
+decimal microseconds = milliseconds * 1000;
+decimal nanoseconds = microseconds * 1000;
+Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = {seconds} seconds = {milliseconds} milliseconds = {microseconds} microseconds = {nanoseconds} nanoseconds");
---
+++ 8 - Data Types and Variables - Exercises/Different Integers Size.cs
@@ -0,0 +1,41 @@
+﻿string number = Console.ReadLine();
+bool isLong = long.TryParse(number, out long longResult);
+if (isLong == false)
+{
+    Console.WriteLine($"{number} can't fit in any type");
+}
+else
+{
+    Console.WriteLine($"{number} can fit in:");
+    bool isSbyte = sbyte.TryParse(number, out sbyte sbyteresult);
+    if (isSbyte)
+    {
+        Console.WriteLine($"* sbyte");
+    }
+    bool isByte = byte.TryParse(number, out byte byteresult);
+    if (isByte)
+    {
+        Console.WriteLine($"* byte");
+    }
+    bool isShort = short.TryParse(number, out short shortresult);
+    if (isShort)
+    {
+        Console.WriteLine($"* short");
+    }
+    bool isUshort = ushort.TryParse(number, out ushort Ushortresult);
+    if (isUshort)
+    {
+        Console.WriteLine($"* ushort");
+    }
+    bool isInt = int.TryParse(number, out int intresult);
+    if (isInt)
+    {
+        Console.WriteLine($"* int");
+    }
+    bool isUint = uint.TryParse(number, out uint uintresult);
+    if (isUint)
+    {
+        Console.WriteLine($"* uint");
+    }
+    Console.WriteLine("* long");
+}
