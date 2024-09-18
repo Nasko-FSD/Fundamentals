6514a2400dbc5da67ba8d89b2b6d0ca743ec2a43
Nasko-FSD <atanasnw@gmail.com>
2024-09-19 00:21:38 +0300

    14 - Lists - Ecerises

---
+++ 14 - Lists - Ecerises/Change List Second Solve.cs
@@ -0,0 +1,83 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+string[] commands = Console.ReadLine()
+    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+static List<int> GetOdds(List<int> numbers, string[] commands)
+{
+    if (commands[0] == "Odd")
+    {
+        numbers.RemoveAll(x => x % 2 == 0);
+    }
+    return numbers;
+}
+
+static List<int> GetEvens(List<int> numbers, string[] commands)
+{
+    if (commands[0] == "Even")
+    {
+        numbers.RemoveAll(x => x % 2 != 0);
+    }
+    return numbers;
+}
+
+static List<int> DeleteNumbers(List<int> numbers, string[] commands)
+{
+    int element = int.Parse(commands[1]);
+    numbers.RemoveAll(x => x == element);
+    return numbers;
+}
+
+static List<int> InsertNumbers(List<int> numbers, string[] commands)
+{
+    int index = int.Parse(commands[2]);
+    int element = int.Parse(commands[1]);
+    numbers.Insert(index, element);
+    return numbers;
+}
+
+static void PrintNumbers(List<int> numbers)
+{
+    Console.WriteLine(string.Join(" ", numbers));
+}
+
+if (string.IsNullOrEmpty(commands[0]))
+{
+    PrintNumbers(numbers);
+}
+else
+{
+    while (commands[0] != "Odd" &&
+           commands[0] != "Even")
+    {
+        if (commands[0] == "Delete")
+        {
+            DeleteNumbers(numbers, commands);
+        }
+        else if (commands[0] == "Insert")
+        {
+            InsertNumbers(numbers, commands);
+        }
+        commands = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        if (commands[0] == "Odd" ||
+            commands[0] == "Even")
+        {
+            break;
+        }
+    }
+}
+
+if (commands[0] == "Odd")
+{
+    GetOdds(numbers, commands);
+    PrintNumbers(numbers);
+}
+else
+{
+    GetEvens(numbers, commands);
+    PrintNumbers(numbers);
+}
---
+++ 14 - Lists - Ecerises/Largest Increasing Subsequence Second Solve.cs
@@ -0,0 +1,40 @@
+﻿var numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+var solutions = new int[numbers.Count];
+var prevSolutions = new int[numbers.Count];
+var maxSolution = 0;
+var maxSolutionIndex = 0;
+for (int current = 0; current < numbers.Count; current++)
+{
+    var solution = 1;
+    var prevIndex = -1;
+    var currenNumber = numbers[current];
+    for (int solIndex = 0; solIndex < current; solIndex++)
+    {
+        var prevNumber = numbers[solIndex];
+        var prevSolution = solutions[solIndex];
+        if (currenNumber > prevNumber
+            && solution <= prevSolution)
+        {
+            solution = prevSolution + 1;
+            prevIndex = solIndex;
+        }
+    }
+    solutions[current] = solution;
+    prevSolutions[current] = prevIndex;
+    if(solution > maxSolution)
+    {
+        maxSolution = solution;
+        maxSolutionIndex = current;
+    }
+}
+Console.WriteLine(maxSolution);
+var index = maxSolutionIndex;
+var result = new List<int>();
+while(index != -1)
+{
+    var currentNumber = numbers[index];
+    result.Add(currentNumber);
+    index = prevSolutions[index];
+}
+result.Reverse();
+Console.WriteLine(string.Join(" ", result));
