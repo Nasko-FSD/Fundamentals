1ec072278f179a28feade898a2c7837672107277
Nasko-FSD <atanasnw@gmail.com>
2024-09-19 09:45:03 +0300

    09 - Methods Debugging and Troubleshooting Code

---
+++ 09 - Methods Debugging and Troubleshooting Code/Calculating Triangle Area.cs
@@ -0,0 +1,9 @@
+﻿CalculatingTriangleArea();
+
+static void CalculatingTriangleArea()
+{
+    double a = double.Parse(Console.ReadLine());
+    double b = double.Parse(Console.ReadLine());
+    double area = (a * b) / 2;
+    Console.WriteLine(area);
+}
---
+++ 09 - Methods Debugging and Troubleshooting Code/Sign of Integer Number.cs
@@ -0,0 +1,22 @@
+﻿int input = int.Parse(Console.ReadLine());
+string sign = GetNumberSign(input);
+Console.WriteLine($"The number {input} is {sign}");
+
+GetNumberSign(input);
+static string GetNumberSign(int input)
+{
+    string result = "";
+    if (input > 0)
+    {
+        result = "positive.";
+    }
+    else if (input < 0)
+    {
+        result = "negative.";
+    }
+    else
+    {
+        result = "zero.";
+    }
+    return result;
+}
